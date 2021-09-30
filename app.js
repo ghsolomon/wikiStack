627ca2f20280b5e45d939129e09b23ee494b103d
Greg Solomon <ghsolomon@gmail.com>
2021-09-30 16:04:56 -0400

    Part 2 complete

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require("express");
-const morgan = require("morgan");
-// const client = require("./db");
+const express = require('express');
+const morgan = require('morgan');
+const layout = require('./views/layout');
+const { db, User, Page } = require('./models');
+db.authenticate()
+  .then(() => {
+    console.log('connected to the db');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to DB', error);
+  });
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+  res.send(layout());
 });
+const init = async () => {
+  await db.sync();
+  const PORT = 1337;
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+init();
---
+++ models/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false,
+});
+const Page = db.define('page', {
+  title: { type: Sequelize.STRING, allowNull: false, unique: true },
+  slug: { type: Sequelize.STRING, allowNull: false, unique: true },
+  content: { type: Sequelize.TEXT, allowNull: false },
+  status: { type: Sequelize.ENUM('open', 'closed') },
+});
+const User = db.define('user', {
+  name: { type: Sequelize.STRING, allowNull: false },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    isEmail: true,
+  },
+});
+module.exports = { db, Page, User };
